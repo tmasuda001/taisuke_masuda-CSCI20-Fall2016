056c67f9b3fd994bb5e9301bb15765c9176d2023
Taisuke Masuda <tmasuda001@student.butte.edu>
2016-08-25 20:59:48 +0000

    ASCII ART FLOWER

---
+++ lab1/lab1.cpp
@@ -0,0 +1,19 @@
+// ASCII ART Flower
+# include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"----^^^^---"<<endl;
+    cout<<"---oooooo--"<<endl;
+    cout<<"--oooooooo-"<<endl;
+    cout<<"--oooooooo-"<<endl;
+    cout<<"---000000--"<<endl;
+    cout<<"---llllll--"<<endl;
+    cout<<"-----()----"<<endl;
+    cout<<"-----()----"<<endl;
+    cout<<"-----()-DD-"<<endl;
+    cout<<"-----()-D--"<<endl;
+    cout<<"-----()D---"<<endl;
+    cout<<"-----()----"<<endl;
+
+}
