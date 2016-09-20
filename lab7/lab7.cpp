e5b22adb12d0ff62ed5b6507a1bfbe3be8b6968e
Taisuke Masuda <tmasuda001@student.butte.edu>
2016-09-20 06:20:11 +0000

    Lab 7 submission

---
+++ lab7/lab7.cpp
@@ -0,0 +1,78 @@
+//Hokey Pokey
+//CSCI-20 Tai Masuda
+//LAB 7
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void chorus1(string side, string body){
+
+    cout << "Put your " << side << " " << body << " in!" << endl;
+    cout << "Put your " << side << " " << body << " out!" << endl;
+    cout << "Pot your " << side << " " << body;
+    cout << " in, and shake it all about!" << endl;
+    cout << "Do the Hokey Pokey and turn yourself around!" << endl; 
+    cout << "Thats that its all about!" << endl;
+}
+    
+void chorus2(string side, string body){
+
+    cout << "Put your " << side << " " << body << " in!" << endl;
+    cout << "Put your " << side << " " << body << " out!" << endl;
+    cout << "Pot your " << side << " " << body;
+    cout << " in, and shake it all about!" << endl;
+    cout << "Do the Hokey Pokey and turn yourself around!" << endl;
+    cout << "Thats that its all about!" << endl;
+}
+
+void chorus3(string side, string body){
+
+    cout << "Put your " << side << " " << body << " in!" << endl;
+    cout << "Put your " << side << " " << body << " out!" << endl;
+    cout << "Pot your " << side << " " << body;
+    cout << " in, and shake it all about!" << endl;
+    cout << "Do the Hokey Pokey and turn yourself around!" << endl;
+    cout << "Thats that its all about!" << endl;
+}
+
+void chorus4(string side, string body){
+
+    cout << "Put your " << side << " " << body << " in!" << endl;
+    cout << "Put your " << side << " " << body << " out!" << endl;
+    cout << "Pot your " << side << " " << body;
+    cout << " in, and shake it all about!" << endl;
+    cout << "Do the Hokey Pokey and turn yourself around!" << endl;
+    cout << "Thats that its all about!" << endl;
+}
+
+void chorus5(string side, string body){
+
+    cout << "Put your " << side << " " << body << " in!" << endl;
+    cout << "Put your " << side << " " << body << " out!" << endl;
+    cout << "Pot your " << side << " " << body;
+    cout << " in, and shake it all about!" << endl;
+    cout << "Do the Hokey Pokey and turn yourself around!" << endl;
+    cout << "Thats that its all about!" << endl;
+}
+
+
+
+int main(){
+    
+    chorus1("Left", "Hand");
+
+    chorus2("Right","Hand");
+    
+    chorus3("Left", "Foot");
+    
+    chorus4("Right", "Foot");
+    
+    chorus5("Whole", "Self");
+    cout << chorus1 << chorus2 << chorus3 << chorus4 << chorus5 << endl;
+    
+
+    
+    
+    
+}//end main
