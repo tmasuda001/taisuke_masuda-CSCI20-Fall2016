c46c983a31ba80fd94d13fb558f7da25e2e494bc
Taisuke Masuda <tmasuda001@student.butte.edu>
2016-10-24 01:59:09 +0000

    Lab 9 submission

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -83,26 +83,26 @@ double TemperatureConverter::GetTempAsFahrenheit(){
     return fahrenheit_;
 }//end get temp as fahrenheit
 
-int main ()
-{
-    TemperatureConverter temp1; //testing default constructor
-    TemperatureConverter temp2(274); //testing overloaded constructor
+// int main ()
+// {
+//     TemperatureConverter temp1; //testing default constructor
+//     TemperatureConverter temp2(274); //testing overloaded constructor
     
-    temp1.PrintTemps();
-    temp2.PrintTemps();
+//     temp1.PrintTemps();
+//     temp2.PrintTemps();
     
-    temp1.SetTempFromKelvin(400.15); //testing mutator function
-    cout<<temp1.GetTempFromKelvin()<<endl;//testing accessor function
-    temp1.PrintTemps();
+//     temp1.SetTempFromKelvin(400.15); //testing mutator function
+//     cout<<temp1.GetTempFromKelvin()<<endl;//testing accessor function
+//     temp1.PrintTemps();
     
-    temp2.SetTempFromCelcius(32); //testing other functions
-    cout<<temp2.GetTempAsCelcius()<<endl;
-    temp2.PrintTemps();
+//     temp2.SetTempFromCelcius(32); //testing other functions
+//     cout<<temp2.GetTempAsCelcius()<<endl;
+//     temp2.PrintTemps();
     
-    temp2.SetTempFromFahrenheit(32);
-    cout<<temp2.GetTempAsFahrenheit()<<endl;
-    temp2.PrintTemps();
+//     temp2.SetTempFromFahrenheit(32);
+//     cout<<temp2.GetTempAsFahrenheit()<<endl;
+//     temp2.PrintTemps();
     
-    return 0;
-}
+//     return 0;
+// }
 
