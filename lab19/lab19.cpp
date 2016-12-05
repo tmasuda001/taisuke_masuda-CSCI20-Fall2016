cea9499e562ff12c198786bbc121635ea6c2138f
Taisuke Masuda <tmasuda001@student.butte.edu>
2016-12-05 18:09:28 +0000

    lab 19 submission

--- lab19/lab19.cpp
+++ lab19/lab19.cpp
@@ -19,13 +19,14 @@
 using namespace std;
 
 int main(){
-    ifstream fsIN;
+    ifstream fsIN;     //Input file stream
+    ofstream fsOUT;    //Outbut file stream
     const int NUM_ROW = 3;
     const int NUM_COL = 4;
-    int array[NUM_ROW][NUM_COL]   = {0};       //Data from file row 1
+    int array[NUM_ROW][NUM_COL] = {0};       //Data from file row 1
     string xFile = "";                 //File for transfering data, input from user.
     
-    cout << "Enter file name: FIXME(ADD IOMANIP INTO LOOPS?) " << endl;
+    cout << "Enter file name: (input.txt) " << endl;
     cin >> xFile;
     
     cout << "Opening file " << xFile << "." << endl;
@@ -48,21 +49,21 @@ int main(){
         row1total += array[0][j];
         
     }
-    cout << "Row 1 sum: " << row1total << endl;
+    //cout << "Row 1 sum: " << row1total << endl;
     
     int row2total = 0;      //row 2 sum
     for(int j = 0; j < NUM_COL; ++j){
         row2total += array[1][j];
         
     }
-    cout << "Row 2 sum: " << row2total << endl;
+    //cout << "Row 2 sum: " << row2total << endl;
     
     int row3total = 0;      //row 3 sum
     for(int j = 0; j < NUM_COL; ++j){
         row3total += array[2][j];
         
     }
-    cout << "Row 3 sum: " << row3total << endl;
+    //cout << "Row 3 sum: " << row3total << endl;
     
 
 
@@ -71,54 +72,60 @@ int main(){
         col1total += array[i][0];
         
     }
-    cout << "Col 1 sum: " << col1total << endl;
+    //cout << "Col 1 sum: " << col1total << endl;
     
     int col2total = 0;      //col 2 sum
     for(int i = 0; i < NUM_ROW; ++i){
         col2total += array[i][1];
         
     }
-    cout << "Col 2 sum: " << col2total << endl;
+    //cout << "Col 2 sum: " << col2total << endl;
     
     int col3total = 0;      //col 3 sum
     for(int i = 0; i < NUM_ROW; ++i){
         col3total += array[i][2];
         
     }
-    cout << "Col 3 sum: " << col3total << endl;
+    //cout << "Col 3 sum: " << col3total << endl;
     
     int col4total = 0;      //col 4 sum
     for(int i = 0; i < NUM_ROW; ++i){
         col4total += array[i][3];
         
     }
-    cout << "Col 4 sum: " << col4total << endl;
-    //}
-    // while(fsIN.is_open()){
-    //     for(int i = 0; i < NUM_ROW; ++i){
-    //         for(int j = 0; j < NUM_COL; ++j){
-    //             cout << array[i][j] << " ";
-    //         }
-    //     }
-    // }
+    //cout << "Col 4 sum: " << col4total << endl;
     
-    //cout << array[0][0];
-    //cout << array[0][1];
-    
-
-        for(int i = 0; i < NUM_ROW; ++i){
-            for(int j = 0; j < NUM_COL; ++j){
+        //Input tester
+        // for(int i = 0; i < NUM_ROW; ++i){
+        //     for(int j = 0; j < NUM_COL; ++j){
                 
-                cout << array[i][j] << " ";
-            }
-        }
+        //         cout << array[i][j] << " ";
+        //     }
+        // }
   
     cout << endl;
-            
-        
+
+
     
+    fsIN.close();           //Close file
     
-    fsIN.close();           //Close file.
+//To output to a file:
+
+//Open file
+fsOUT.open("myOutFile.txt");
+
+if(!fsOUT.is_open()){
     
+    cout << "Outputfile could not be opened. (myOutFile.txt)" << endl;
+    return 1;
+}
+//Output/write to file
+    fsOUT << setw(4) << array[0][0] << setw(4) << array[0][1] << setw(4) << array[0][2] << setw(4) << array[0][3] << setw(4) << row1total << endl;
+    fsOUT << setw(4) << array[1][0] << setw(4) << array[1][1] << setw(4) << array[1][2] << setw(4) << array[1][3] << setw(4) << row2total << endl;
+    fsOUT << setw(4) << array[2][0] << setw(4) << array[2][1] << setw(4) << array[2][2] << setw(4) << array[2][3] << setw(4) << row3total << endl;
+    fsOUT << setw(4) << col1total << setw(4) << col2total << setw(4) << col3total << setw(4) << col4total << endl;
+
+//Close file.
+fsOUT.close();
 
 }//End main
